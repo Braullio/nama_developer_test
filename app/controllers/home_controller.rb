3f6e51090c10d01deeab71294354452738a8407a
braullio <braulliovg@gmail.com>
2020-10-15 02:25:44 -0300

    create controller home

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+    # flash['danger'] = 'activerecord.errors.messages.permitted'
+    # flash['success'] = 'activerecord.errors.messages.permitted'
+  end
+
+  def new; end
+
+  def create; end
+end
